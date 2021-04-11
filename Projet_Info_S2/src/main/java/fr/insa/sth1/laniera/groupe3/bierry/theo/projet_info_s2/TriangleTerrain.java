d85379bfc29201bad73de7702d6486be973ad960
theob <theob@THEO>
2021-04-11 09:37:59 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

---
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/TriangleTerrain.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
+
+/**
+ *
+ * @author theob
+ */
+public class TriangleTerrain {
+    
+    private int Id;
+    private Segment [] Seg = new Segment[3]; // On stocke ici les 3 segments qui forment notre triangle terrain
+    
+    
+}
