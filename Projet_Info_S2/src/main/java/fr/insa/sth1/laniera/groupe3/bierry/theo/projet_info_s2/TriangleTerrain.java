89bc6d58635377010a6739b579ed68b1019daf50
theob <theob@THEO>
2021-04-11 09:45:19 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

--- Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/TriangleTerrain.java
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/TriangleTerrain.java
@@ -15,11 +15,34 @@ public class TriangleTerrain {
     private Segment [] Seg = new Segment[3]; // On stocke ici les 3 segments qui forment notre triangle terrain
     
     public TriangleTerrain(int Iden, Segment[] S){ //On envoit les 3 segment à travers un tableau, c'est plus simple
+        if (S.length !=3) {
+            if (S.length < 3) {
+                throw new Error ("Il n'y a pas assez de segment");
+            }
+            else if (S.length>3) {
+                throw new Error ("Il y a trop de segment pour definir un TriangleTerrain");
+            }
+        }
         Id = Iden;
         for (int i = 0; i < 3; i++) {
             Seg[i] = S[i];
             
         }
     }
+
+   
+    public int getId() {
+        return Id;
+    }
+
+    public Segment[] getSeg() { //Ici on retourne tout le tableau des segments
+        return Seg;
+    }
+    
+    public Segment getSeg(int i){ //Ici, on ne retourne que un seul segment précisé
+        return Seg[i];
+    }
+    
+    
     
 }
