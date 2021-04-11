20e79039b627d59fcfba9d808eb7631b2e600a1b
theob <theob@THEO>
2021-04-11 11:31:33 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

--- Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Barre.java
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Barre.java
@@ -14,13 +14,16 @@ public class Barre {
     private int Id;
     private Noeud [] Noeuds_Barre;
     private Treillis Treillis_Barre;
+    private TypeBarre Type_de_Barre;
     
-    public Barre(int Iden, Noeud N1, Noeud N2, Treillis T){
+    public Barre(int Iden, Noeud N1, Noeud N2, Treillis T, TypeBarre TB){
         Id = Iden;
         Noeuds_Barre = new Noeud[2];
         Noeuds_Barre[0] = N1;
         Noeuds_Barre[1] = N2;
         Treillis_Barre = T;
+        Type_de_Barre = TB;
+        TB.addBarre_TypeBarre(this); // On complète les listes de chaque élements
         N1.addBarre(this);
         N2.addBarre(this);
         T.addBarre_Treillis(this);
