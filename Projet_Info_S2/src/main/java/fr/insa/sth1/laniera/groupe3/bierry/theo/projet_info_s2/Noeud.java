9bc42b983fa8f0cf2f0ea749f2a4665f1bd5d1c6
theob <theob@THEO>
2021-04-11 11:11:26 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

--- Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Noeud.java
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Noeud.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 public abstract class Noeud {
     
     private int Id;
-    private ArrayList<Barre> Barres_Noeud;
+    protected ArrayList<Barre> Barres_Noeud;
     
     public abstract void addBarre(Barre B);//{Barres_Noeud.add(B);}
     
