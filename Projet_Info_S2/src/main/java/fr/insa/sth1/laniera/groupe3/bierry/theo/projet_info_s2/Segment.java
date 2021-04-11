b6bba19145910ee99b8748f5e69bed2ae64b5518
theob <theob@THEO>
2021-04-11 09:31:43 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

---
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Segment.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
+
+/**
+ *
+ * @author theob
+ */
+public class Segment {
+    
+    private int Id;
+    private Point[] Extremite; // Tableau ou l'on met les points des extrémités du segment
+    
+    public Segment (int Iden, Point P1, Point P2){
+        Id = Iden;
+        Extremite[0] = P1; //On rempli le tableau des extremité avec les points P1 et P2
+        Extremite[1] = P2;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public Point[] getExtremite() {
+        return Extremite;
+    }
+    
+    public Point getExtremite(int Pos){
+        return Extremite[Pos];
+    }
+    
+    
+}
