aeeb712224c3adfdcc54f7a528767f8d29488e3d
theob <theob@THEO>
2021-04-11 10:45:31 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

--- Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Treillis.java
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Treillis.java
@@ -5,10 +5,38 @@
  */
 package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author theob
  */
 public class Treillis {
+    
+    private ArrayList <Barre> Barre_Treillis;
+    private ArrayList <Noeud> Noeuds_Treillis;
+    private int Id;
+    
+    public Treillis (int Iden){
+        Id = Iden;
+        Barre_Treillis = new ArrayList<>();
+        Noeuds_Treillis = new ArrayList<>();
+    }
+    
+    public ArrayList<Barre> getBarre_Treillis(){
+        return Barre_Treillis;
+    }
+    
+    public ArrayList<Noeud> getNoeuds_Treillis(){
+        return Noeuds_Treillis;
+    }
+    
+    public void addBarre_Treillis(Barre B){ //Ceci permet d'ajouter des barres dans notre list de barres
+        Barre_Treillis.add(B);
+    }
+    
+    public void addNoeuds_Treillis(Noeud N){ //Ceci permet de rajouter des noeuds dans notre list de noeuds
+        Noeuds_Treillis.add(N);
+    }
     
 }
