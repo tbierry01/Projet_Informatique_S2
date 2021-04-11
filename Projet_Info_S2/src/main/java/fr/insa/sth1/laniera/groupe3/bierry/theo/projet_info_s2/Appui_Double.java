73214005c5ebe9f87b4e6e16e634f04dd4a97d16
theob <theob@THEO>
2021-04-11 11:13:28 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

--- Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Appui_Double.java
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Appui_Double.java
@@ -10,5 +10,10 @@ package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
  * @author theob
  */
 public class Appui_Double extends Appui{
-    
+    //TODO Constructeurs
+    @Override
+    public void addBarre(Barre B) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        super.Barres_Noeud.add(B);
+    }
 }
