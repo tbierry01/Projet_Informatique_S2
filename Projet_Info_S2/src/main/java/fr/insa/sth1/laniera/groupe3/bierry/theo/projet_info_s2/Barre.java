407f227810a7cef08f25822a699bae3f52976ecb
theob <theob@THEO>
2021-04-11 10:35:23 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

---
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Barre.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
+
+/**
+ *
+ * @author theob
+ */
+public class Barre {
+    private int Id;
+    private Noeud [] Noeuds_Barre;
+    private Treillis Treillis_Barre;
+    
+    
+}
