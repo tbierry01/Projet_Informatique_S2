276de65ab25c4807f1e3d4d46cd481e7cfb2b861
theob <theob@THEO>
2021-04-11 09:20:20 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

---
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Appui_Simple.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
+
+/**
+ *
+ * @author theob
+ */
+public class Appui_Simple extends Appui{
+    
+}
