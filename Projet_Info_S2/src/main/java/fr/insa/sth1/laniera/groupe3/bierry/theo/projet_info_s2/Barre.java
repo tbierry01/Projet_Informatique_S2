f0d41c338e820ce87a950a76406ecfe6524d56c6
theob <theob@THEO>
2021-04-11 11:21:02 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

--- Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Barre.java
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Barre.java
@@ -10,8 +10,19 @@ package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
  * @author theob
  */
 public class Barre {
+    
     private int Id;
     private Noeud [] Noeuds_Barre;
     private Treillis Treillis_Barre;
     
+    public Barre(int Iden, Noeud N1, Noeud N2, Treillis T){
+        Id = Iden;
+        Noeuds_Barre = new Noeud[2];
+        Noeuds_Barre[0] = N1;
+        Noeuds_Barre[1] = N2;
+        Treillis_Barre = T;
+        N1.addBarre(this);
+        N2.addBarre(this);
+        T.addBarre_Treillis(this);
+    }
 }
