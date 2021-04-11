d4261f859398f0380c8e8f3394696201273a96d8
theob <theob@THEO>
2021-04-11 11:23:07 +0200

    Merge origin/main into main
    Conflicts:
    	Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Class_Test.java

--- Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Appui_Simple.java
+++ Projet_Info_S2/src/main/java/fr/insa/sth1/laniera/groupe3/bierry/theo/projet_info_s2/Appui_Simple.java
@@ -10,7 +10,7 @@ package fr.insa.sth1.laniera.groupe3.bierry.theo.projet_info_s2;
  * @author theob
  */
 public class Appui_Simple extends Appui{
-    //TODO Constructeurs
+    //TODO Constructeurs Avec dedans le treillis, en même temps le noeud doit s'ajouter au tableua du treillis
     @Override
     public void addBarre(Barre B) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
